fcaf20979ae17c3bcb5e21530610280f41e2f873
Kelston Burgess <121816599+KelsDRK@users.noreply.github.com>
2024-03-21 18:49:31 +0000

    FMXL with entry screen

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.sw2.c195 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.sw2.c195 to javafx.fxml;
+    exports com.sw2.c195;
+    exports controller;
+    opens controller to javafx.fxml;
+}
